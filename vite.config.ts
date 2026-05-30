e925d13789128186dd8a87754342b5d54cdc61d4
unknown <pedrosoaress365@gmail.com>
2026-05-29 21:52:21 -0300

    configurando deploy automatico

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/",
+  base: "/dra.laura/",
   server: {
     port: 3000,
   }
